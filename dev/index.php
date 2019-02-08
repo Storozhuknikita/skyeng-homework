666fc3582b74b845c6b0be2e8998056e6b48e194
Storozhuk Nikita <storozhuk.nikita@gmail.com>
2019-02-08 17:25:39 +0300

    Add dev

--- dev/index.php
+++ dev/index.php
@@ -1,7 +1,31 @@
 <?php
+function bigSum(string $x1, string $x2): string
+{
+    $arrX1 = str_split($x1);
+    $arrX2 = str_split($x2);
+    if (count($arrX1) >= count($arrX2)) {
+        $i = count($arrX1);
+    } else {
+        $i = count($arrX2);
+    }
+    $n = 0;
+    $arrayResult = [];
+    for ($x = 0; $x < $i; $x++) {
+        $result = array_pop($arrX1) + array_pop($arrX2) + $n;
+        $str = (string)$result;
+        if ($result >= 10) {
+            $n = 1;
+        } else {
+            $n = 0;
+        }
+        array_unshift($arrayResult, $str[$n]);
+    }
+    if ($n == 1) {
+        array_unshift($arrayResult, $n);
+    }
+    return implode($arrayResult);
+}
 
-
-
-
+echo bigSum('111111111111111111111111111111111111111111111', '111111111111111111111111111111111111111111111');
 
 ?>
